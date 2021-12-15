d96789bb7447daee431beb8bb3b67c043ce2b0ea
Felipe Augustos <felipe_augustos84@outlook.com.br>
2021-12-15 19:11:40 -0300

    feat: adding the mirage and some configs to api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { http } from "./services/api";
 
-const App = () => (
-  <div>
-    <p>Iniciando projeto</p>
-  </div>
-);
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    http.get("/users").then(({ data }) => setUsers(data.users));
+    console.log(users);
+  }, []);
+
+  return (
+    <div>
+      <p>Iniciando projeto</p>
+    </div>
+  );
+};
 
 export default App;
